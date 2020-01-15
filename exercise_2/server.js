5e363b793efe2b51cd4574e5ead8f9fc2c79e0a9
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2020-01-15 16:12:31 +0000

    Creating a node server

--- exercise_2/server.js
+++ exercise_2/server.js
@@ -1,1 +1,18 @@
-let http = require("http")
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+//add the router
+app.use('/', router);
+app.use(express.static(__dirname));
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');

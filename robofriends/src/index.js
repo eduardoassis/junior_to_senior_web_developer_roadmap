e8fed941ef466a4779198dc63761b751eb24f360
Eduardo Silva <eduardo.silva.ext@nokia.com>
2020-10-22 11:18:11 +0100

    Adding Card component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './components/hello/Hello';
+import Card from './components/card/Card';
 import 'tachyons'
+import { robots } from './components/robots'
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );

6d6057d13d456eb92321cd6e694bc14183a50a8d
Eduardo Silva <eduardo.silva.ext@nokia.com>
2020-10-22 18:12:53 +0100

    Creating App component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons'
-import { robots } from './components/robots'
-import CardList from './components/card-list/CardList';
+import App from './components/app/App';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );

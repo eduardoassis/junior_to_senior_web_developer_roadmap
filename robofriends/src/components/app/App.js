415011c04815864c091a2c9a47a6e6b4eff6e606
Eduardo Silva <eduardo.silva.ext@nokia.com>
2020-10-25 18:19:13 +0000

    Scroll component

--- robofriends/src/components/app/App.js
+++ robofriends/src/components/app/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import SearchBox from '../search-box/SearchBox';
 import CardList from './../card-list/CardList';
-
 import './App.css';
+import Scroll from './../scroll/Scroll';
 
 class App extends Component {
 
@@ -39,7 +39,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox placeholder='Seacrh robots' onSearchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }

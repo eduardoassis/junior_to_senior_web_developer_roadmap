d3ae3dfe7be2321091df808fde5d11c3892dc9f1
ASSIS SILVA Eduardo <eduardo.assissilva@altran.com>
2020-11-11 17:52:25 +0000

    Adding redux logger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import 'tachyons'
 import App from './containers/app/App';
